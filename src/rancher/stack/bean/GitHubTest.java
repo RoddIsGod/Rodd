0559cbf167e89f9c08a97653c3d581031081d7fd
Rodd(wang jian) <jian.wang2@hpe.com>
2016-07-25 17:17:00 +0800

    update test

---
+++ src/rancher/stack/bean/GitHubTest.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package rancher.stack.bean;
+
+/**
+ * @ClassName: GitHubTest.java
+ * @Description: TODO
+ * @author: Rodd(Wang,Jian)
+ * @email jian.wang2@hpe.com
+ * @date Jul 25, 2016 5:16:36 PM
+ */
+public class GitHubTest {
+
+}
